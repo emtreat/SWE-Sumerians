3f09fdf358526606ec212b60ed726f5c04a856d4
Kenneth Eze <kennetheze530@gmail.com>
2025-03-08 14:58:26 -0600

    not working front end, open to debugging

--- front_end/react_app/vite-app/src/App.jsx
+++ front_end/react_app/vite-app/src/App.jsx
@@ -1,74 +1,108 @@
 import './App.css'
+import React, { useState, useEffect } from 'react';
 
-function FileRow({ file }) {
-  const name = <span style={{ color: 'white' }}>
-      {file.name}
-    </span>;
+const GetUsers = () => {
+  const [posts, setPosts] = useState([]);
+   useEffect(() => {
+      fetch("http://localhost:8080/api/users")
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
 
-  return (
-    <tr>
-      <td>{name}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ files }) {
-  const rows = [];
-
-  files.forEach((file) => {
-    rows.push(
-      <FileRow
-      file={file}
-        key={file.name} />
-    );
-  });
-
-  return (
-    <table>
-      <thead>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-    </form>
-  );
-}
-
-function FilterableProductTable({ files }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable files={files} />
+   return (
+    <div className="container">
+       {posts.map((post) => {
+          return (
+            <div className="container" key={post._id}>
+              console.log(key);
+                <h2 className="name">{post.name}</h2>
+                console.log(name);
+                <div className="button">
+                <div className="delete-btn">Delete</div>
+                </div>
+             </div>
+          );
+       })}
     </div>
-  );
+    );
 }
 
-const FILES = [
-  {name: "file_1.jpeg"},
-  {name: "file_2.jpeg"},
-  ];
+// function FileRow({ file }) {
+//   const name = <span style={{ color: 'white' }}>
+//       {file.name}
+//     </span>;
+
+//   return (
+//     <tr>
+//       <td>{name}</td>
+//     </tr>
+//   );
+// }
+
+// function ProductTable({ files }) {
+//   const rows = [];
+
+//   files.forEach((file) => {
+//     rows.push(
+//       <FileRow
+//       file={file}
+//         key={file.name} />
+//     );
+//   });
+
+//   return (
+//     <table>
+//       <thead>
+//       </thead>
+//       <tbody>{rows}</tbody>
+//     </table>
+//   );
+// }
+
+// function SearchBar() {
+//   return (
+//     <form>
+//       <input type="text" placeholder="Search..." />
+//     </form>
+//   );
+// }
+
+// function FilterableProductTable({ files }) {
+//   return (
+//     <div>
+//       <SearchBar />
+//       <ProductTable files={files} />
+//     </div>
+//   );
+// }
+
+// const FILES = [
+//   {name: "file_1.jpeg"},
+//   {name: "file_2.jpeg"},
+//   ];
 
 function App() {
-  const handleClick = () => {
-    alert('File Added!');
-  };
+  // const handleClick = () => {
+  //   alert('File Added!');
+  // };
 
   return (
     <div className="app">
-      <div className="name">User's Name</div>
-      <div className="container">
-        <button onClick={handleClick}>Add File</button>
+    {/* //   <div className="name">User's Name</div>
+    //   <div className="container">
+    //     <button onClick={handleClick}>Add File</button>
+      // </div>
+      <div> */}
+      <h1>Did it run?</h1>
+        <GetUsers />
       </div>
-      <div className="container">
-        <FilterableProductTable files={FILES} />
-      </div>
-    </div>
+    // </div>
   );
 }
 
