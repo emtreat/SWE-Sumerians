22115d37469f475f5caf6994a5cdbf848a1601c2
redhulkgod <johntristanedwards@gmail.com>
2025-04-09 23:48:20 -0500

    added drop box feature

--- front_end/react_app/vite-app/src/App.jsx
+++ front_end/react_app/vite-app/src/App.jsx
@@ -1,18 +1,19 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from './pages/Home'
-import  { Login } from './pages/Login'
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { DropBox } from "./components/Dropbox";
 
 function App() {
-    return (
-      <Router>
-        <Routes>
-        <Route path="/" element={<Login/>} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/Home" element={<Home />} />
+        {/* <Route path="/DropBox" element={<DropBox />} /> */}
       </Routes>
-      </Router>
-      );
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
