9b52fb81d2823352d5f600cb280767d241d109d9
redhulkgod <johntristanedwards@gmail.com>
2025-04-23 18:38:43 -0500

    removed AddBlob.jsx for being unused

--- front_end/react_app/vite-app/src/components/AddBlob.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import axios from "axios";
-
-export function PostFileBlob(userListOfBlobs) {
-  // given already format checked list of blobs
-  // send post to backend
-  // possibly send back status
-}
