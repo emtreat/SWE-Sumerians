fd660b0369454c6a14395e884df8b667a7a04f43
redhulkgod <johntristanedwards@gmail.com>
2025-04-26 20:57:31 -0500

    Things modified:     -AddFile now accepts file structure that includes fileNmae, filesize, fileblob     -fileblob added to file structure     -GetFiles modified for new File struct     - new function GetFile added         -used for getting file information and blob     - altered DropBox to send in correct format         -Dropbox send binary Array Buffer instead of BinaryURL

--- front_end/react_app/vite-app/src/components/GetUser.jsx
+++ front_end/react_app/vite-app/src/components/GetUser.jsx
@@ -34,6 +34,8 @@ export function GetUsers() {
   if (error) return <div>Error: {error}</div>;
   if (!user) return <div>No user data found</div>;
 
+  // console.log(user.files);
+
   return (
     <div
       style={{
