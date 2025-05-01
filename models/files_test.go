de7b33e05de05db1c98177b0fb11ed4e9fd21c8c
emtreat <ebenezer211@icloud.com>
2025-05-01 12:30:12 -0500

    User Tests and general api_test structure (currently getting error 500) on tests

--- models/files_test.go
+++ models/files_test.go
@@ -2,103 +2,70 @@ package models_test
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	"github.com/emtreat/SWE-Sumerians/models"
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var collection_test *mongo.Collection
 
-type TestEnv struct {
-    files models.FileModel
-}
 
-func ServerTest() (*fiber.App, *mongo.Collection, error) {
-    err := godotenv.Load("../.env")
 
-    if err != nil {
-        fmt.Println("Test environment setup error")
-        return nil, nil, err
-    }
-
-    URI := os.Getenv("URI")
-
-    dbOpts := options.Client().ApplyURI(URI)
-    db, err := mongo.Connect(context.Background(), dbOpts)
-
-    if err != nil {
-        fmt.Println("Error test connecting")
-        return nil, nil, err
-    }
-
-    collection_test := db.Database("project_db").Collection("test_users")
-
-    _, err = collection_test.DeleteMany(context.Background(), bson.M{})
-
-    if err != nil {
-        fmt.Println("error deleting")
-        return nil, nil, err
-    }
+/*
+func TestGetFile() (t *testing.T) {
 
     env := &TestEnv{
         files: models.FileModel{DB: collection_test},
     }
 
     app := fiber.New()
+    app.Post("/", env.files.GetFile)
 
-    app.Post("/api/users/:email/files", env.files.AddFile)
-    return app, collection_test, nil
-}
+    req, err := http.NewRequest(http.MethodGet, "/", nil)
+    if err != nil {
+        t.Fatalf("Couldn't create get request: %v\n", err)
+    }
+
+    w := httptest.NewRecorder()
+
+    app.Test()
+
+}*/
 
 func TestFileFunctions(t *testing.T) {
     // Test server values
-    app, collection, err := ServerTest()
+    app := fiber.New()
+    defer app.Shutdown()
 
-    if err != nil {
-        fmt.Println("Test server didn't start")
-        t.FailNow()
+    env := &TestEnv{
+        files: models.FileModel{DB: collection_test},
     }
 
+    app.Post("/test", env.files.AddFile)
 
-    defer collection.Database().Client().Disconnect(context.Background())
-
-    defer collection.DeleteMany(context.Background(), bson.M{})
-
-
-    // Test adding a File TODO
+    // Test adding a File 
     testFile := map[string] interface{} {
         "ID": "TestID",
         "FileName": "TestFileName",
         "FileSize": 34,
-        "FileBlob": 01001,
+        "FileBlob": 0xa20c,
     }
-
     fileData, _ := json.Marshal(testFile) // convert to json
-
     // Check Post
-    req := httptest.NewRequest(http.MethodPost, "/api/users/:email/files", bytes.NewReader(fileData))
-
-    req.Header.Set("Content-Type", "application/json")
+    req := httptest.NewRequest(http.MethodGet, "/test", bytes.NewReader(fileData))
+//    req.Header.Set("Content-Type", "application/json")
 
     resp, err := app.Test(req)
+    print(resp.StatusCode)
 
     assert.Nil(t, err)
     assert.Equal(t, http.StatusCreated, resp.StatusCode)
 
     // Check Get
-
 
     // Check Delete
 }
